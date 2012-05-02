96496e339e7c77259889ace0302c21b1dab16b4e
Jed Parsons <jparsons@mozilla.com>
2012-05-02 16:31:30 -0700

    API change; can specify arbitrary id providers

--- example/main.js
+++ example/main.js
@@ -3,7 +3,7 @@
 const
 express = require('express'),
 path = require('path'),
-browserid = require('../lib/browserid');
+BrowserID = require('../lib/browserid');
 
 var exampleServer = express.createServer();
 
@@ -14,21 +14,29 @@ exampleServer
 
 var localOrigin;
 
+/* In this example, we make it possible to authenticate through two 
+ * providers: the real browserid, and mockmyid, which will issue an
+ * assertion for any mockmyid.com email address.
+ */
+var options = {issuers: ['browserid.org', 'mockmyid.com']};
+var browserid = new BrowserID(options);
+
 exampleServer.post('/auth', function(req, res, next) {
   if (!req.body || !req.body.assertion) {
     res.writeHead(400);
     res.end();
   } else {
-    browserid({
+    browserid.verify({
       audience: localOrigin,
       assertion: req.body.assertion
     }, function(err, r) {
       if (err) {
+        console.error(err);
         res.writeHead(500);
         res.write(err);
         res.end();
       } else {
-        res.write(JSON.stringify(r.email));
+        res.write(JSON.stringify(r));
         res.end();
       }
     });
